ea54d74389c9ae202a3aaf4c80a13d47a57143b1
Pablo Reyes <pabloandresreyes2004@gmail.com>
2023-05-10 21:12:22 -0500

    Create product

---
+++ Models/Providers.cs
@@ -0,0 +1,13 @@
+﻿namespace SupermarketWEB.Models
+{
+    public class Providers
+    {
+        public int Id { get; set; }
+        public string ProviderName { get; set; }
+        public int Document { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+       
+
+    }
+}
